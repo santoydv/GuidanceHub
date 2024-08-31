900655dcd4930db41af43f8f925c20a460393115
Muhammad Ashif Raza <mdashifraza222jj@gmail.com>
2024-08-31 16:21:49 +0530

    Updated

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <section className="px-40">
       <div className="flex items-center gap-5">
-        <Image src="/Academics-Badge.png" alt="" width={400} height={100} />
+        <Image src="/academics.svg" alt="" width={400} height={100} />
         <div className="flex flex-col gap-5">
           <h1 className="text-4xl font-bold uppercase tracking-wider">Academic</h1>
           <p className="text-2xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi soluta cum, excepturi numquam molestias facilis eaque corrupti earum odit, deserunt libero voluptatibus cupiditate doloremque quisquam blanditiis. Temporibus ut atque illo!</p>
@@ -43,7 +43,7 @@ export default function Home() {
           <Link href="/alumni-interaction">
             <button className="px-3 py-2 bg-white border-2 text-xl border-blue-600 w-[10rem] hover:bg-blue-600 hover:text-white">Know More</button>
           </Link>        </div>
-        <Image src="/Academics-Badge.png" alt="" width={400} height={100} />
+        <Image src="/alumni-interaction.svg" alt="" width={400} height={100} />
       </div>
     </section>
   );
