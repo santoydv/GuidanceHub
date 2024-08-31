f292701b095da073d229fd9aeb07ad16b460cf21
Muhammad Ashif Raza <mdashifraza222jj@gmail.com>
2024-09-01 00:41:26 +0530

    Updated

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
     <>
       <section id="hero-section" className="h-[85vh] w-full px-32 flex items-center">
         <div className="flex flex-col gap-3">
-          <h1 className="text-[#F8D287] text-8xl font-bold capitalize">GuidanceHUB</h1>
+          <h1 className="text-[#F8D287] text-8xl font-bold capitalize text-sh">GuidanceHUB</h1>
           <p className="text-4xl text-white max-w-[40rem] text-wrap">One Stop Solution for Success in Academics, Skills and Beyond.</p>
         </div>
       </section>
@@ -32,20 +32,15 @@ export default function Home() {
               <h1 className="capitalize text-white text-5xl  font-bold ">WECLOME TO GUIDANCEHUB</h1>
             </div>
             <div className="h-[75vh] w-full bg-[#F8D287] grid grid-cols-2">
-              <div className="my-10 mx-32 flex justify-center  flex-col gap-5">
+              <div className="mx-32 flex justify-center  flex-col gap-5">
                 <h2 className="text-[#3F51B5] text-3xl">A One Stop Solution for Fresher Students.</h2>
                 <p className="text-[#3F51B5] text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui minima velit quo nostrum reprehenderit porro, non architecto ipsa maiores excepturi commodi a, illo sit suscipit nobis, exercitationem magni corporis accusantium.</p>
                 <div>
                   <button className="bg-[#3F51B5] px-3 py-2 text-white ">Know More</button>
                 </div>
               </div>
-              <div>
-                <Image
-                  src="/boyphoto.webp"
-                  alt=""
-                  width={700}
-                  height={700}
-                />
+              <div className="h-full w-full">
+                <img src="/about-sec.jpg" className="object-cover w-full h-full" alt="" />
 
               </div>
             </div>
@@ -55,13 +50,13 @@ export default function Home() {
 
         <div className="bg-white pb-10">
           <div className="py-5">
-            <h2 className="text-[#3F51B5] text-5xl font-bold px-12">Our Features</h2>
+            <h2 className="text-[#3F51B5] text-5xl font-bold px-9">Our Features</h2>
           </div>
           <div className="grid grid-cols-4 px-10 gap-5">
             <div className="flex flex-col gap-5">
               <div className="w-[22rem] h-[17rem] relative overflow-hidden group">
-                <img src="/boyphoto.webp" className="w-full h-full object-cover" alt="Boy Photo" />
-                <div className="absolute px-2 text-[#3F51B5] top-[90%] bg-white h-full w-full transition-all duration-500 ease-in-out group-hover:top-0 flex flex-col gap-5">
+                <img src="/academics.jpg" className="w-full h-full object-cover" alt="Boy Photo" />
+                <div className="absolute px-2 text-[#3F51B5] top-[85%] bg-white h-full w-full transition-all duration-500 ease-in-out group-hover:top-0 flex flex-col gap-5">
                   <h1 className="text-2xl  font-bold">Academics</h1>
                   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit fugit, quas deleniti provident unde repudiandae inventore dolorem? Numquam labore quae, culpa, assumenda velit rerum sapiente praesentium adipisci repellat itaque voluptates.</p>
                 </div>
@@ -70,9 +65,9 @@ export default function Home() {
             </div>
             <div className="flex flex-col gap-5">
               <div className="w-[22rem] h-[17rem] relative overflow-hidden group">
-                <img src="/boyphoto.webp" className="w-full h-full object-cover" alt="Boy Photo" />
-                <div className="absolute text-[#3F51B5] px-2 top-[90%] bg-white h-full w-full transition-all duration-500 ease-in-out group-hover:top-0 flex flex-col gap-5">
-                  <h1 className="text-2xl  font-bold">UpSkill</h1>
+                <img src="/up-skilling.jpg" className="w-full h-full object-cover" alt="Boy Photo" />
+                <div className="absolute text-[#3F51B5] px-2 top-[85%] bg-white h-full w-full transition-all duration-500 ease-in-out group-hover:top-0 flex flex-col gap-5">
+                  <h1 className="text-2xl  font-bold">Up-Skilling</h1>
                   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit fugit, quas deleniti provident unde repudiandae inventore dolorem? Numquam labore quae, culpa, assumenda velit rerum sapiente praesentium adipisci repellat itaque voluptates.</p>
                 </div>
               </div>
@@ -80,9 +75,9 @@ export default function Home() {
             </div>
             <div className="flex flex-col gap-5">
               <div className="w-[22rem] h-[17rem] relative overflow-hidden group">
-                <img src="/boyphoto.webp" className="w-full h-full object-cover" alt="Boy Photo" />
-                <div className="absolute text-[#3F51B5] px-2 top-[90%] bg-white h-full w-full transition-all duration-500 ease-in-out group-hover:top-0 flex flex-col gap-5">
-                  <h1 className="text-2xl  font-bold">Club Activities</h1>
+                <img src="/club-guidance.jpg  " className="w-full h-full object-cover" alt="Boy Photo" />
+                <div className="absolute text-[#3F51B5] px-2 top-[85%] bg-white h-full w-full transition-all duration-500 ease-in-out group-hover:top-0 flex flex-col gap-5">
+                  <h1 className="text-2xl  font-bold">Club's Guidance</h1>
                   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit fugit, quas deleniti provident unde repudiandae inventore dolorem? Numquam labore quae, culpa, assumenda velit rerum sapiente praesentium adipisci repellat itaque voluptates.</p>
                 </div>
               </div>
@@ -90,9 +85,9 @@ export default function Home() {
             </div>
             <div className="flex flex-col gap-5">
               <div className="w-[22rem] h-[17rem] relative overflow-hidden group">
-                <img src="/boyphoto.webp" className="w-full h-full object-cover" alt="Boy Photo" />
-                <div className="absolute px-2 text-[#3F51B5] top-[90%] bg-white h-full w-full transition-all duration-500 ease-in-out group-hover:top-0 flex flex-col gap-5">
-                  <h1 className="text-2xl  font-bold">Alumni Intraction</h1>
+                <img src="/mentorship.jpg" className="w-full h-full object-cover" alt="Boy Photo" />
+                <div className="absolute px-2 text-[#3F51B5] top-[85%] bg-white h-full w-full transition-all duration-500 ease-in-out group-hover:top-0 flex flex-col gap-5">
+                  <h1 className="text-2xl  font-bold">Mentorship</h1>
                   <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit fugit, quas deleniti provident unde repudiandae inventore dolorem? Numquam labore quae, culpa, assumenda velit rerum sapiente praesentium adipisci repellat itaque voluptates.</p>
                 </div>
               </div>
@@ -101,29 +96,35 @@ export default function Home() {
 
           </div>
         </div>
-        <footer className="bg-[#3F51B5] text-white grid grid-cols-3 py-14 justify-items-start">
-          <div className="h-full flex px-10 items-center justify-center text-3xl font-bold uppercase tracking-wider text-white">
-            GuidanceHUB
+        <footer className="bg-[#3F51B5]">
+          <div className=" text-white flex justify-start py-14">
+            <div className="h-full flex-1 px-10 flex items-center justify-center text-3xl font-bold uppercase tracking-wider text-white">
+              GuidanceHUB
+            </div>
+            <div className="flex-1">
+              <h2 className="font-bold mb-2 text-xl ">Useful Links</h2>
+              <ul className="flex flex-col justify-start gap-2">
+                <Link href="/"><li className="hover:text-gray-200">Home</li></Link>
+                <Link href="/about"><li className="hover:text-gray-200">About us</li></Link>
+                <Link href="/contact"><li className="hover:text-gray-200">Contact us</li></Link>
+                <Link href="/privacy-policy"><li className="hover:text-gray-200">Privacy Policy</li></Link>
+                <Link href="/disclaimer"><li className="hover:text-gray-200">Disclaimer</li></Link>
+              </ul>
+            </div>
+            <div className="flex-1">
+              <h2 className="font-bold mb-2 text-xl ">Our Features</h2>
+              <ul className="flex flex-col justify-start gap-2">
+                <Link href="/"><li className="hover:text-gray-200">Home</li></Link>
+                <Link href="/about"><li className="hover:text-gray-200">About us</li></Link>
+                <Link href="/contact"><li className="hover:text-gray-200">Contact us</li></Link>
+                <Link href="/privacy-policy"><li className="hover:text-gray-200">Privacy Policy</li></Link>
+                <Link href="/disclaimer"><li className="hover:text-gray-200">Disclaimer</li></Link>
+              </ul>
+            </div>
           </div>
-          <div>
-            <ul>
-              <Link href="/"><li>Home</li></Link>
-              <Link href="/about"><li>About us</li></Link>
-              <Link href="/contact"><li>Contact us</li></Link>
-              <Link href="/privacy-policy"><li>Privacy Policy</li></Link>
-              <Link href="/disclaimer"><li>Disclaimer</li></Link>
-            </ul>
-          </div>
-          <div>
-            <ul>
-              <Link href="/"><li>Home</li></Link>
-              <Link href="/about"><li>About us</li></Link>
-              <Link href="/contact"><li>Contact us</li></Link>
-              <Link href="/privacy-policy"><li>Privacy Policy</li></Link>
-              <Link href="/disclaimer"><li>Disclaimer</li></Link>
-            </ul>
-          </div>
+          <div className="text-center py-2 text-white">Copyright &#169; GuidanceHUB. All Right Reserved</div>
         </footer>
+
 
       </section>
     </>
--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -2,15 +2,15 @@ import Link from "next/link"
 
 const Header = () => {
   return (
-    <header className="flex items-center gap-24  h-[5rem]  bg-[#F8D287] z-10">
+    <header className="flex items-center gap-24  h-[5rem]  bg-[#F8D287] sticky top-0 z-10">
         <div className="bg-[#3F51B5] h-full flex px-10 items-center justify-center text-3xl font-bold uppercase tracking-wider text-white">GuidanceHUB</div>
         <div>
             <nav className="list-none flex gap-20 text-[#3F51B5] font-semibold ">
               <Link href="/"><li className="hover:text-blue-950">Home</li></Link>  
               <Link href="#about-us"><li className="hover:text-blue-950">About Us</li></Link>  
-              <Link href="/"><li className="hover:text-blue-950">UpSkill</li></Link>  
-              <Link href="/"><li className="hover:text-blue-950">Club Activities</li></Link>  
-              <Link href="/"><li className="hover:text-blue-950">Alumni Intraction</li></Link>  
+              <Link href="/"><li className="hover:text-blue-950">Contact Us</li></Link>  
+              <Link href="/"><li className="hover:text-blue-950">Privacy Policy</li></Link>  
+              <Link href="/"><li className="hover:text-blue-950">Disclaimer</li></Link>  
             </nav>
         </div>
     </header>
