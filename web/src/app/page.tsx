0bab8561c2958e4ce58ffc743cba9dcb2082a610
Muhammad Ashif Raza <mdashifraza222jj@gmail.com>
2024-08-31 23:11:14 +0530

    Updated

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 
 export default function Home() {
 
@@ -6,12 +7,12 @@ export default function Home() {
       <section id="hero-section" className="h-[85vh] w-full px-32 flex items-center">
         <div className="flex flex-col gap-3">
           <h1 className="text-[#F8D287] text-8xl font-bold capitalize">GuidanceHUB</h1>
-          <p className="text-4xl text-white">One Stop Solution.</p>
+          <p className="text-4xl text-white max-w-[40rem] text-wrap">One Stop Solution for Success in Academics, Skills and Beyond.</p>
         </div>
       </section>
       <section className="bg-[#FFFFFF]">
-        <div className="flex ">
-          <div className="w-[15rem] bg-white flex flex-col gap-5 m-5 items-center">
+        <div className="flex mb-10" id="#about-us">
+          {/* <div className="w-[15rem] bg-white flex flex-col gap-5 m-5 items-center">
             <div className="flex w-[10rem] h-[10rem] rounded-xl justify-center items-center  border-2 border-[#3F51B5] text-xl text-center">
               Academic
             </div>
@@ -24,19 +25,79 @@ export default function Home() {
             <div className="flex w-[10rem] h-[10rem] rounded-xl justify-center items-center  border-2 border-[#3F51B5] text-xl text-center">
               Alumni Interaction
             </div>
-          </div>
-          <div className="flex flex-col w-full">
+          </div> */}
+          <div className="flex flex-col w-full overflow-hidden" >
             <div className="bg-[#3F51B5] h-[10rem] w-full flex justify-center items-center">
               <h1 className="capitalize text-white text-5xl  font-bold ">WECLOME TO GUIDANCEHUB</h1>
             </div>
             <div className="h-[75vh] w-full bg-[#F8D287] grid grid-cols-2">
-                <div className="my-10 mx-32 flex justify-center items-center flex-col gap-5">
-                  <h2 className="text-[#3F51B5] text-3xl">A One Stop Solution for Fresher Students.</h2>
-                  <p className="text-[#3F51B5] text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui minima velit quo nostrum reprehenderit porro, non architecto ipsa maiores excepturi commodi a, illo sit suscipit nobis, exercitationem magni corporis accusantium.</p>
-                  <button>Know More</button>
+              <div className="my-10 mx-32 flex justify-center  flex-col gap-5">
+                <h2 className="text-[#3F51B5] text-3xl">A One Stop Solution for Fresher Students.</h2>
+                <p className="text-[#3F51B5] text-lg">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui minima velit quo nostrum reprehenderit porro, non architecto ipsa maiores excepturi commodi a, illo sit suscipit nobis, exercitationem magni corporis accusantium.</p>
+                <div>
+                  <button className="bg-[#3F51B5] px-3 py-2 text-white ">Know More</button>
                 </div>
-                <div></div>
+              </div>
+              <div>
+                <Image
+                  src="/boyphoto.webp"
+                  alt=""
+                  width={700}
+                  height={700}
+                />
+
+              </div>
             </div>
+          </div>
+        </div>
+
+
+        <div className="bg-white pb-10">
+          <div className="py-5">
+            <h2 className="text-[#3F51B5] text-5xl font-bold px-12">Our Features</h2>
+          </div>
+          <div className="grid grid-cols-4 px-10 gap-5">
+            <div className="flex flex-col gap-5">
+              <div className="w-[22rem] h-[17rem] relative overflow-hidden group">
+                <img src="/boyphoto.webp" className="w-full h-full object-cover" alt="Boy Photo" />
+                <div className="absolute px-2 top-[90%] bg-white h-full w-full transition-all duration-500 ease-in-out group-hover:top-0 flex flex-col gap-5">
+                  <h1 className="text-2xl  font-bold">Academics</h1>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit fugit, quas deleniti provident unde repudiandae inventore dolorem? Numquam labore quae, culpa, assumenda velit rerum sapiente praesentium adipisci repellat itaque voluptates.</p>
+                </div>
+              </div>
+              <button className="py-2 px-3 border-2 border-[#3F51B5] text-[#3F51B5] font-bold hover:bg-[#3F51B5] transition-all duration-200 hover:text-white text-xl outline-none">View more</button>
+            </div>
+            <div className="flex flex-col gap-5">
+              <div className="w-[22rem] h-[17rem] relative overflow-hidden group">
+                <img src="/boyphoto.webp" className="w-full h-full object-cover" alt="Boy Photo" />
+                <div className="absolute px-2 top-[90%] bg-white h-full w-full transition-all duration-500 ease-in-out group-hover:top-0 flex flex-col gap-5">
+                  <h1 className="text-2xl  font-bold">UpSkill</h1>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit fugit, quas deleniti provident unde repudiandae inventore dolorem? Numquam labore quae, culpa, assumenda velit rerum sapiente praesentium adipisci repellat itaque voluptates.</p>
+                </div>
+              </div>
+              <button className="py-2 px-3 border-2 border-[#3F51B5] text-[#3F51B5] font-bold hover:bg-[#3F51B5] transition-all duration-200 hover:text-white text-xl outline-none">View more</button>
+            </div>
+            <div className="flex flex-col gap-5">
+              <div className="w-[22rem] h-[17rem] relative overflow-hidden group">
+                <img src="/boyphoto.webp" className="w-full h-full object-cover" alt="Boy Photo" />
+                <div className="absolute px-2 top-[90%] bg-white h-full w-full transition-all duration-500 ease-in-out group-hover:top-0 flex flex-col gap-5">
+                  <h1 className="text-2xl  font-bold">Club Activities</h1>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit fugit, quas deleniti provident unde repudiandae inventore dolorem? Numquam labore quae, culpa, assumenda velit rerum sapiente praesentium adipisci repellat itaque voluptates.</p>
+                </div>
+              </div>
+              <button className="py-2 px-3 border-2 border-[#3F51B5] text-[#3F51B5] font-bold hover:bg-[#3F51B5] transition-all duration-200 hover:text-white text-xl outline-none">View more</button>
+            </div>
+            <div className="flex flex-col gap-5">
+              <div className="w-[22rem] h-[17rem] relative overflow-hidden group">
+                <img src="/boyphoto.webp" className="w-full h-full object-cover" alt="Boy Photo" />
+                <div className="absolute px-2 top-[90%] bg-white h-full w-full transition-all duration-500 ease-in-out group-hover:top-0 flex flex-col gap-5">
+                  <h1 className="text-2xl  font-bold">Alumni Intraction</h1>
+                  <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit fugit, quas deleniti provident unde repudiandae inventore dolorem? Numquam labore quae, culpa, assumenda velit rerum sapiente praesentium adipisci repellat itaque voluptates.</p>
+                </div>
+              </div>
+              <button className="py-2 px-3 border-2 border-[#3F51B5] text-[#3F51B5] font-bold hover:bg-[#3F51B5] transition-all duration-200 hover:text-white text-xl outline-none">View more</button>
+            </div>
+
           </div>
         </div>
       </section>
